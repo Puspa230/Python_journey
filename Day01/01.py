0ff7bab490c0de0b937092c939f0262d989c5fa4
Puspita Moharana <moharanapuspita230@gmail.com>
2026-07-14 11:47:55 +0530

    Stop tracking Day01 and Day02

--- Day01/01.py
+++
@@ -1,5 +0,0 @@
-from datetime import date
-
-print("Name: Puspita Moharana")
-print("Date: ", date.today())
-print("Goal: To become a Python Developer in 90 days.")
--- Day02/conditional_statement.py
+++
@@ -1,9 +0,0 @@
-user_input = input("Enter your Score: ")
-score = int(user_input)
-
-if score >= 90:
-    print("GRADE A")
-elif score >=50:
-    print("GRADE B")
-else: 
-    print("FAIL")
